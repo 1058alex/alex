f0f7c59c508a2b6296ba1e55e62d1d29e32dcacb
1058alex <1058alex@gmail.com>
2023-06-14 15:39:25 +0200

    ///

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-yuggugyuc
-1212121212
+#
 
-=======
-lalala:l
+
+
+
+
+
+
+text = input('Enter your text: ')
+first_spase = text.find(' ')
+last_spase = text.rfind(' ')
+word1 = text[:first_spase]
+word2 = text[first_spase+1:last_spase]
+word3 = text[last_spase+1:]
+print(word1)
+print(word2)
+print(word3)
+
