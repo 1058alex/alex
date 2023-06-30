8e737afb1859e3231d9148b78b3ce3d74f19e194
1058alex <1058alex@gmail.com>
2023-06-30 16:19:23 +0200

    Shorted method_3

--- 3-3.py
+++ 3-3.py
@@ -5,5 +5,5 @@ method_1 = f'Hi, my name is {name}, I am {age} years old, I live in {city}'
 print(method_1)
 method_2 = 'Hi, my name is ' + name + ', I am ' + age + ' yars old, I live in ' + city + '.'
 print(method_2)
-method_3 = f'Hi, my name is {name}, I am {age} years old, I live in {city}'.format(name=name, age=age, city=city)
+method_3 = 'Hi, my name is {name}, I am {age} years old, I live in {city}'.format(name=name, age=age, city=city)
 print(method_3)
